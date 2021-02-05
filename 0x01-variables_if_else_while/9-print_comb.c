792ee7bc9a94bb72cb89aeb89ffe5257064ba345
anamariaroman <2979@holbertonschool.com>
2021-02-04 23:34:19 -0500

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - print numbers
+ * description: use while
+ * Return: void
+ */
+int main(void)
+{
+	int a < (48);
+
+	while (a < 58)
+	{
+		putchar(a);
+		if (a < 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		a++;
+	}
+	putchar(10);
+	return (0);
+
+}
