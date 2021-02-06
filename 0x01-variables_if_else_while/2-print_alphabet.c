bed33dc07289e6c8b69bc756f4e6d2264ec12bfc
anamariaroman <2979@holbertonschool.com>
2021-02-06 00:09:52 -0500

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print alphabet
+ * description: use while
+ * Return: void
+ */
+
+int main(void)
+{
+	int a = 97;
+
+	while (a < 122)
+	{
+		putchar(a);
+		if (a < 122)
+		a++;
+	}
+		putchar(10);
+	return (0);
+}
