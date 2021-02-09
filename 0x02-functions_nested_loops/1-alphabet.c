9d139182b2a76512adc390c04d4c1692a42f892c
anamariaroman <2979@holbertonschool.com>
2021-02-09 16:24:48 -0500

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,15 +4,13 @@
  * print_alphabet - short description, single line
  * main - Alphabetic Holberton
  * Description: a-z alphabet holberton
- * print_alphabet
  * Return: void
- */
+*/
 
 void print_alphabet(void)
 {
-	
 	int az;
-	
+
 	for (az = 97; az <= 122; az++)
 		_putchar(az);
 
