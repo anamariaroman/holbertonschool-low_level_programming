fb27621bf634cd2edd8583c31c31e48040bd5184
anamariaroman <2979@holbertonschool.com>
2021-02-09 15:05:35 -0500

    task 0

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * main - puts out 'Holberton' followed by a new line
+ * Use the _putchar
+ *
+ * Return: zero
+ */
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
+}
