abb07fc38a5b3322911ad21afcd4bb1f5f97758b
anamariaroman <2979@holbertonschool.com>
2021-02-11 23:27:02 -0500

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,13 +11,13 @@ void more_numbers(void)
 {
 	int i, b;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		for (b = 0; b <= 14; b++)
 		{
 			if (b >= 10)
 			{
-				_putchar(i / 10 + '0');
+				_putchar(b / 10 + '0');
 			}
 			_putchar(b % 10 + '0');
 		}
