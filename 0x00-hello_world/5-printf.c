8fc2a0089f123e6affc9f4855b92d27ea8ed0967
anamariaroman <2979@holbertonschool.com>
2021-02-13 12:47:49 -0500

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - puts line
+ * Description: uses puts show text
+ * Return: void
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
