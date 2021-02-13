180725eb83a0bbc1aa9fec8ea87b6976d4e4e3cb
anamariaroman <2979@holbertonschool.com>
2021-02-13 15:04:36 -0500

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - alphabet 3
+ * Description: delete two letters
+ * Return: void
+*/
+
+int main(void)
+{
+	char a;
+
+	for (a = 'a' ; a <= 'z' ; a++)
+	{
+		if (a != 'e' && a != 'q')
+		{
+			putchar(a);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
