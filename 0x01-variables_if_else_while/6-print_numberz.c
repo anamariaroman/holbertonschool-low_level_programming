d8fec3c7b673cfbaf9620ee4729741c1544da824
anamariaroman <2979@holbertonschool.com>
2021-02-13 15:37:42 -0500

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers (0 - 10) without char
+ *
+ * Return: void
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 48; num <= 57; num++)
+	{
+		putchar(num);
+	}
+	putchar(10);
+	return (0);
+}
