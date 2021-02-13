8bdf84e4582157c92ab137ee3ad6d0538abd542e
anamariaroman <2979@holbertonschool.com>
2021-02-13 12:39:39 -0500

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - use puts
+ * Description: uses puts to output fixed message
+ * Return: void
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
