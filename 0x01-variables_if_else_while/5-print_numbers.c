25c29fc18ca5c60f77615fd47067275e0f1fd411
anamariaroman <2979@holbertonschool.com>
2021-02-13 15:27:26 -0500

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print numbers
+ * Description - print numbers
+ * Return: void
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 9 ; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
