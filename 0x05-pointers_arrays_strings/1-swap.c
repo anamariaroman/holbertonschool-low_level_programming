2b6203f0c2b7990f5178f2f16950043d26c68c87
anamariaroman <2979@holbertonschool.com>
2021-02-15 23:55:10 -0500

    task

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * swap_int - swaps the
+ * @a: first int to swap
+ * @b: second int to swap
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
