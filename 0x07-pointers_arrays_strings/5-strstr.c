409358ffd117e7bc34bbc500675634597c36b34b
anamariaroman <2979@holbertonschool.com>
2021-02-22 23:50:48 -0500

    task

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,24 +1,32 @@
 #include "holberton.h"
+
 /**
- * _strstr - world
- * @haystack: char1
- * @needle: char2
- * Return: char
-*/
+ * _strstr - locates a substring
+ * @haystack: string in which to check for needle
+ * @needle: substring to find in haystack
+ *
+ * Return: pointer to beginning of needle in haystack or NULL if no match
+ */
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j;
+	unsigned int i = 0, j = 0;
 
-	for (i = 0 ; haystack[i] != '\0' ; i++)
+	while (haystack[i])
 	{
-		for (j = 0 ; needle[j] != '\0' ; j++)
+		while (needle[j] && (haystack[i] == needle[0]))
 		{
-			if (haystack[i] == 32)
-			{
-				i++;
-				return (haystack + i);
-			}
+			if (haystack[i + j] == needle[j])
+				j++;
+			else
+				break;
 		}
+		if (needle[j])
+		{
+			i++;
+			j = 0;
+		}
+		else
+			return (haystack + i);
 	}
-	return ('\0');
+	return (0);
 }
