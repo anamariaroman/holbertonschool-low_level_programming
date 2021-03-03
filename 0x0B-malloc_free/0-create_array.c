be0752faaa80b29f866ada1b5ba6112c88c6d4e5
anamariaroman <2979@holbertonschool.com>
2021-03-02 23:13:11 -0500

    main

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ * create_array - function that creates an array of chars
+ * @size: number of arguments
+ * @c: char to specify
+ *
+ * Return: char
+*/
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *a;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	a = malloc(size * sizeof(char));
+	if (a == NULL)
+	{
+		return (0);
+	}
+	for (i = 0; i < size; i++)
+	{
+		a[i] = c;
+	}
+	return (a);
+}
