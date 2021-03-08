04be058e3b3c48fad96f1eb71c7155bdba8dcaba
anamariaroman <2979@holbertonschool.com>
2021-03-08 18:01:50 -0500

    task

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - information dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* _DOG_H_  */
