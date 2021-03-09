c4d187986d030fa1dea43abab612da18395d2b1d
anamariaroman <2979@holbertonschool.com>
2021-03-08 22:20:42 -0500

    task

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * new_dog - copy a new dog
+ * @name: name
+ * @age: dog age
+ * @owner: dog own
+ *
+ * Return: copy a new dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int i, a, k, m;
+	dog_t *dog;
+
+	if (name == NULL || owner == NULL)
+		return (NULL);
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+	for (i = 0; name[i] != '\0'; i++)
+	{
+	}
+	i++;
+	dog->name = malloc(i * sizeof(char));
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	for (a = 0; owner[a] != '\0'; a++)
+	{
+	}
+	a++;
+	dog->owner = malloc(a * sizeof(char));
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	for (k = 0; k < i; k++)
+		dog->name[k] = name[k];
+	dog->age = age;
+	for (m = 0; m < a; m++)
+		dog->owner[m] = owner[m];
+	return (dog);
+}
