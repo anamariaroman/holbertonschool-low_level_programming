123adec4f0f3a274969d02b79455211125b3477f
anamariaroman <2979@holbertonschool.com>
2021-03-19 21:13:07 -0500

    task

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,48 +1,51 @@
 #include "lists.h"
-#include <stdio.h>
-
 /**
-* add_node_end - print lenght the struct list
-* @head: ptr structure
-* @str: string arg
-* Return: list
-*/
-
+ * add_node_end - add node to the end
+ * @head: first node
+ * @str: string
+ * Return: adress
+ */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	int i;
-	list_t *node;
-	list_t *temp;
+	list_t *ptr, *otherP;
+
+	ptr = malloc(sizeof(list_t));
+	if (ptr == NULL)
+	{
+		return (0);
+	}
+	ptr->str = strdup(str);
+	if (ptr->str == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	ptr->len = _strlen(ptr->str);
+	ptr->next = NULL;
+	if (*head == NULL)
+	{
+		*head = ptr;
+		return (ptr);
+	}
+	otherP = *head;
+	while (otherP->next)
+	{
+		otherP = otherP->next;
+	}
+	otherP->next = ptr;
+	return (ptr);
+}
+/**
+ * _strlen - length of str
+ * @str: str
+ * Return: length
+ */
+unsigned int _strlen(char *str)
+{
+	unsigned int i;
 
 	for (i = 0; str[i]; i++)
 	{
 	}
-
-	node = malloc(sizeof(list_t));
-
-	if (node == NULL)
-	{
-		return (NULL);
-	}
-
-	node->str = strdup(str);
-	node->len = i;
-	node->next = NULL;
-
-	if (*head == NULL)
-	{
-		*head = node;
-		return (NULL);
-	}
-
-	temp = *head;
-
-	while (temp->next != NULL)
-	{
-		temp = temp->next;
-	}
-
-	temp->next = node;
-
-	return (node);
+	return (i);
 }
