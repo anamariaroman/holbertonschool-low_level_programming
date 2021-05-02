5bebbec249da3ff0ecb7c3d22dfac5d39af08916
anamariaroman <2979@holbertonschool.com>
2021-05-01 23:59:02 -0500

    task update

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 /**
  * dlistint_len - returns the number of elements in a linkedlist.
- * @h: pointer
+ * @j: pointer
  * Return: number of elements in a linkedlist.
  */
 size_t dlistint_len(const dlistint_t *j)
