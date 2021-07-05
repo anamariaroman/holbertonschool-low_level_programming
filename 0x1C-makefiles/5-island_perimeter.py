45f305f60ecfe083a281e5ed9a349a49cee97b67
anamariaroman <2979@holbertonschool.com>
2021-07-04 23:42:03 -0500

    tasks update and remove problems

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" island perimeter """
+
+
+def island_perimeter(grid):
+    """ Return the perimeter island """
+
+    len_x = len(grid)
+    len_y = len(grid[0])
+    sum = 0
+    for i in range(len_x):
+        for a in range(len_y):
+            if grid[i][a] == 1:
+                sum = sum + 4
+                if i > 0:
+                    sum = sum - grid[i - 1][a]
+                if i + 1 < len(grid):
+                    sum = sum - grid[i + 1][a]
+                if a > 0:
+                    if grid[i][a - 1] == 1:
+                        sum = sum - 1
+                if a + 1 < len(grid[i]):
+                    sum = sum - grid[i][a + 1]
+                    sum +1
+    return sum
